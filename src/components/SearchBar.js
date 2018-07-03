a4af7b87a657f7872e6c3357253ec57896f5452d
engelsun <artyom.romenskiy@gmail.com>
2018-07-03 09:59:23 +0300

    add Provider

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    constructor() {
+        super();
+        this.inputValue = "";
+    }
     render() {
         return (
             <div className="ui icon input fluid">
-                <input className="prompt" type="text"
+                <input className="prompt" onInput={this.handleInput.bind(this)} type="text"
                        placeholder="Enter some info about client to search..."/>
                 <i className="search icon"/>
             </div>
         )
+    }
+
+    handleInput(e) {
+        this.inputValue = e.target.value;
+
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from './App';
+import allReducers from "./store/reducers/allReducers";
+
+const store = createStore(allReducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
