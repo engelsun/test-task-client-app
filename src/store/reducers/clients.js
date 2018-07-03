7d96ff1ed35ac123af714b312aafa49ab870d139
engelsun <artyom.romenskiy@gmail.com>
2018-07-04 01:25:24 +0300

    Updates

--- src/store/reducers/clients.js
+++
@@ -1,8 +0,0 @@
-import {ADD_CLIENTS, ADD_SELECTED_CLIENT} from "../actions";
-
-export default function (state = [], action) {
-    if (action.type === ADD_CLIENTS) {
-        state = [...state, ...action.payload];
-    }
-    return state;
-};
--- src/store/reducers/selectedClient.js
+++
@@ -1,8 +0,0 @@
-import {ADD_SELECTED_CLIENT} from "../actions";
-
-export default function (state = [], action) {
-    if (action.type === ADD_SELECTED_CLIENT) {
-        state = action.payload;
-    }
-    return state;
-};
