022a5ae9b8abdfc606c8c09816e51ab2ac059767
engelsun <artyom.romenskiy@gmail.com>
2018-07-03 13:21:10 +0300

    update view

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import ClientList from "./components/ClientList";
 import SearchBar from "./components/SearchBar";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <SearchBar/>
-        <ClientList/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="ui equal width centered grid container">
+                <div className="stretched row">
+                    <div className=" six wide column">
+                        <div className="ui segment">
+                            <SearchBar/>
+                            <ClientList/>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="ui segment">
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
